86f1e86dfeada3e832587c522bb62f1f949fa09c
foha67 <141177426+foha67@users.noreply.github.com>
2023-08-07 12:25:08 -0400

    Update models.py

--- littlelemon/littlelemon/workspace/lemontree/restaurant/models.py
+++ littlelemon/littlelemon/workspace/lemontree/restaurant/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    menu_item_description = models.TextField(max_length=1000, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    first_name = models.CharField(max_length=255)
+    reservation_date = models.DateField()
+    reservation_slot = models.SmallIntegerField(default=10)
+
+    def __str__(self): 
+        return self.first_name
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
